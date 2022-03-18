b9fc380411a1432e7f27e093fcc73f34a2eaa193
emmankoko <emmankoko519@gmail.com>
2022-03-18 00:14:40 +0000

    more loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - checks for a digit
- * @c: an integer parameter for the function
+ * @c: an integer argument for the function
  * Return: 1 if c is digit and 0 if otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c<= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -15,3 +15,4 @@ void print_numbers(void)
 		_putchar(i + '0');
 	}
 	_putchar('\n');
+}
