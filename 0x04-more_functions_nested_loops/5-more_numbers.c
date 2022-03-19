105a1ca78783e32bf2e74a94510e10ef5b71ebaa
emmankoko <emmankoko519@gmail.com>
2022-03-19 00:27:24 +0000

    more loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,16 @@ void more_numbers(void)
 		int j;
 
 		for (j = 0; j <= 14; j++)
-			_putchar(j + '0');
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar(j + '0');
+		}
 		_putchar('\n');
 
 	}
-	_putchar('\n');
 }
