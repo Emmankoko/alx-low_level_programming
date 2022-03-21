111baad52d8543c66afae7325333ec91e69dccef
emmankoko <emmankoko519@gmail.com>
2022-03-21 15:52:32 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#incude "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to char
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	int i, len, half;
+	char temp;
+
+	for (len = 0; s[len] != '0'; len++);
+
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
