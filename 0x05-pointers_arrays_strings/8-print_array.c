a50e4c210cea9824195a25005383286d70d7b688
emmankoko <emmankoko519@gmail.com>
2022-03-21 20:45:46 +0000

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *@a: pointer to int
- *@n: argument passed for number of elements to be printed
+ * print_array - prints n elemments of an array of integers
+ * @a: int type array pointer
+ * @n: argument passed for number of elements to be printed
  *Description: Numbers should be seperated by space and comma
  *Numbers should be displayed in the same order they are in the array
  *Return: void
