c7347edbed8018859d13f43b7c3126de398bcd23
emmankoko <emmankoko519@gmail.com>
2022-03-21 16:03:07 +0000

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string starting with first character 
+ * puts2 - prints every other character of a string starting with the first
  * @str: pointer to char
  * Return: void
  */
