efc76ff6600c650920a1d0f5d6ae60c874397e95
Emmankoko <emmankoko519@gmail.com>
2022-10-05 14:35:17 +0200

    linear search implementation

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+/**
+ * main - Entry point
+ * linear_search - function that the returns index at which value can be found
+ * value - the number being searched for
+ * size - size of the array
+ * array - array containing the elements to be searched for
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    int i = 0;
+    while(i < size && array[i] != value)
+    {
+        printf("value checked array [%d] = [%d]\n", i, array[i]);
+        i++;
+    }
+    if(i == size)
+    {
+        return -1;
+    }
+    else
+    {
+        printf("value checked array [%d] = [%d]\n", i, array[i]);
+        return i;
+    }
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+This is a collection of the implementation of the search algorithms
+the linear search, the binary search, and the jump sorts 
+and also their space complexities
